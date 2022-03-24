dda9e19fd67e9ee319cab00168e997091a645f86
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-03-24 18:42:01 +0700

    added ui, and new item

---
+++ app.js
@@ -0,0 +1,35 @@
+//* Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//* Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+//* Functions
+function addTodo(event){
+    event.preventDefault();
+    console.log('hello');
+
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');    
+
+    const newTodo = document.createElement('li')
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn')
+    todoDiv.appendChild(completedButton);
+
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+
+    todoList.appendChild(todoDiv);
+
+    todoInput.value = "";
+}
