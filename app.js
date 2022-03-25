1d4a21eb58f29beed04272729f67bd365b47c64d
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-03-25 13:10:49 +0700

    little customization

--- app.js
+++ app.js
@@ -38,6 +38,9 @@ function deleteCheck(e) {
 	if (item.classList[0] === "complete-btn") {
 		const todo = item.parentElement;
 		todo.classList.toggle("completed");
+		todo.addEventListener("transitioned", function(){
+			todo.classList.toggle("completed");
+		})
 		saveLocalTodos(new TODOItem(todo.children[0].innerText, COMPLETED));
 	}
 }
